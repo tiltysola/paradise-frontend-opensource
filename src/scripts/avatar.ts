78bb3cc45dc9d0212d26293a49bdc80bc3f8399a
TiltySola <tilty.sola@gmail.com>
2023-10-10 23:54:50 +0800

    fix: some bugs

--- src/scripts/avatar.ts
+++ src/scripts/avatar.ts
@@ -2,7 +2,7 @@ export const getUserAvatar = (userId: string, avatar?: string) => {
   if (/^https?/g.test(avatar || '')) {
     return avatar;
   } else if (avatar) {
-    return `/user/avatar?userId=${userId}&avatar=${avatar}`;
+    return `/api/user/avatar?userId=${userId}&avatar=${avatar}`;
   } else {
     return '/images/tilty_qq_avatar.png';
   }
